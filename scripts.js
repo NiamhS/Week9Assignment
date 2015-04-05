5cdd2aeafd944935c0e9f6c2e61ae18f859c6bb1
NiamhS <sweeneyniamh@gmail.com>
2015-04-05 12:26:36 -0400

    Committing all files created in class for guardianjQuery assignment.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+for (var i=0; i<data.length; i++) {
+	
+	var rowData = data[i];
+	
+	var template = '<div class="quoteContainer">'+
+				'<div class="mainQuote">For the past 26 years I've thought of heroin every day</div>'+
+				'<div class="wholeQuote">But never thought of using it. It never made anything better, just worse. Look, every junky is always "considering" getting clean. Every junky wants to be clean. Every using junky also wants more heroin. Call it a contradiction call it what you want. But no junky wants the life they have.</div>'+
+				'<div class="source">James</div>'+
+				'<div class="yearsClean">26 years clean</div>'+	
+				'<div class="location">Sydney, Australia</div>'+
+				</div>+
+}
