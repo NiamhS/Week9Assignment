f2893a1827b3bf31d7f5a06ff2d0853cb6ecca7f
NiamhS <sweeneyniamh@gmail.com>
2015-04-12 16:55:45 -0400

    Trying again to commit changed style for name, location and years clean to .details

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ for (var i=0; i<guardianHeroinData.length; i++) {
 						'<div class = "readMore">Read More</div>'+
 						'<div class = "moreInfo">'+
 							'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
-							'<div class="source">'+rowData.source+'</div>'+
-							'<div class="yearsClean">'+rowData.yearsClean+'</div>'+	
-							'<div class="location">'+rowData.location+'</div>'+
+							'<div class="details">'+rowData.source+'</div>'+
+							'<div class="details">'+rowData.yearsClean+'</div>'+	
+							'<div class="details">'+rowData.location+'</div>'+
 					'</div>'+
 				'</div>');
 				
